3afe47d37fb365b7d4a0b5042285d5c477ea339e
Roberto <mirandaroberto1994@gmail.com>
2023-09-13 09:25:24 -0300

    agrega  archivos a gitignore

--- app/controllers/servidor_controller.py
+++
@@ -1,8 +0,0 @@
-from app.models.servidor import Servidor
-
-
-class ServidorController:
-    @classmethod
-    def traer_todos(cls):
-        servidores = Servidor.obtener_servidores()
-        return servidores, 200
--- app/routes/servidor_bp.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from ..controllers.servidor_controller import ServidorController
-
-servidor_bp = Blueprint('servidor_bp', __name__)
-
-servidor_bp.route('/', methods=['GET'])(ServidorController.traer_todos)
