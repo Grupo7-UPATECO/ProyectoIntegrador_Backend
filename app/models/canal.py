a8e94bef4e8ef410fcdd1bf720cc4a459c09ebf7
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2023-09-17 16:33:53 -0300

    quito classmethod

--- app/models/canal.py
+++ app/models/canal.py
@@ -2,7 +2,6 @@ from app.database import DatabaseConnection
 
 class Canal:
 
-    @classmethod
 
     def __init__(self, id_canal=None, nombre_canal=None, id_servidor=None ):
         self.id_canal = id_canal
